a54093363722cb0d981e11e13a541eb116cda335
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-18 17:54:44 +0530

    component created

---
+++ src/app/messages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'messages',
+  template: '<p> This is my message </p>'
+  styleUrls: ['./app.component.css']
+})
+export class MessagesComponent {
+  title = 'my app';
+}
