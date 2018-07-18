e750a3dac303c24606faea49e600ab7fbf2a6230
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-18 18:05:22 +0530

    custom data added

--- src/app/messages.component.ts
+++ src/app/messages.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'messages',
-  template: '<p> This is my message </p>'
-  styleUrls: ['./app.component.css']
+  template: '<p> This is my message </p> <div *ngFor="let message of messages">{{message.text}} by: {{message.owner}} </div>'
+
 })
 export class MessagesComponent {
-  title = 'my app';
+  messages = [{text:'Some text', owner: 'Tim'}, {text: 'other message', owner: 'Jane'}];
 }
